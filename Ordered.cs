7cbdbb288605a58a99b61823af29130c314f3875
castelom <vitorcastelo.m@gmail.com>
2024-02-08 14:09:15 -0300

    Create Project

---
+++ Ordered.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Castelom.Sorting_Algorithms
+{
+    internal class Ordered
+    {
+        public int[] BubbleSort(int[] array)
+        {
+            for(int i = 0; i < array.Length-1; i++)
+            {
+                for (int j = 1;j < array.Length-i; j++)
+                {
+                    if (array[j-1] > array[j])
+                    {
+                        int aux = array[j-1];
+                        array[j-1] = array[j];
+                        array[j] = aux;
+                    }
+                }
+            }
+            return array;
+        }
+
+        public void printArray(int[] array)
+        {
+            for(int i =0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Castelom.Sorting_Algorithms;
+
+int[] array = { 5, 4, 3, 7, 8, 9, 10 };
+Ordered order = new Ordered();
+array = order.BubbleSort(array);
+
+order.printArray(array);
